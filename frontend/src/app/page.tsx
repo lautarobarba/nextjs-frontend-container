46b12a0ae2de3c505af3ac1294e8866bb88bd9fa
Lautaro Barba <lau_mb94@hotmail.com>
2024-07-05 17:18:38 -0300

    Se quito pagina extra

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,20 +1,9 @@
-import Link from "next/link";
 
 const Home = () => {
   return (
     <main>
       <br />
-      <h1 className="underline decoration-sky-500">Frontend Mentor</h1>
-      <div className="flex flex-col flex-nowrap justify-center">
-        <br />
-        <hr className="m-auto w-80" />
-        <Link
-          href="/results-summary-component"
-          className="m-auto text-blue-500"
-        >
-          Results summary component
-        </Link>
-      </div>
+      <h1 className="text-center">NextJS Template</h1>
     </main>
   );
 };
